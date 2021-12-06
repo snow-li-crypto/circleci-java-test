50ea2938a277a6a40dba772b337b8970372e1223
joe.li <527112461@qq.com>
2021-12-06 18:32:39 +0800

    Integration sonarcloud

--- src/test/java/cn/servcie/OrderServiceTest.java
+++ src/test/java/cn/servcie/OrderServiceTest.java
@@ -1,7 +1,12 @@
 package cn.servcie;
 
+import cn.Application;
 import cn.service.OrderService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * 〈OrderService testcase〉
@@ -10,12 +15,15 @@ import org.junit.Test;
  * @create 2021/11/22
  * @since 1.0.0
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
 public class OrderServiceTest {
 
+    @Autowired
+    private OrderService orderService;
 
     @Test
-    public void test(){
-        OrderService orderService = new OrderService();
+    public void test() {
         orderService.test();
     }
 
