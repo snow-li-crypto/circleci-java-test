69127b8a4cd2a5471ccde54ae027fa369aa5c604
joe.li <527112461@qq.com>
2022-01-05 17:27:53 +0800

    Integration sonarcloud

---
+++ module2/src/main/java/cn/module2/Module2Service.java
@@ -0,0 +1,16 @@
+package cn.module2;
+
+/**
+ * 〈〉
+ *
+ * @author snow.li
+ * @create 2022/1/5
+ * @since 1.0.0
+ */
+public class Module2Service {
+
+    public void test() {
+        System.out.println("test module2");
+    }
+    
+}
