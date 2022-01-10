7b5b87901d34e45f5f27b56c8a210394d53c8d71
joe.li <527112461@qq.com>
2022-01-10 08:54:37 +0800

    Integration sonarcloud

---
+++ module4/src/main/java/com/module4/module4Service3.java
@@ -0,0 +1,15 @@
+package com.module4;
+
+/**
+ * 〈〉
+ *
+ * @author snow.li
+ * @create 2022/1/7
+ * @since 1.0.0
+ */
+public class module4Service3 {
+
+    public void test2() {
+        System.out.println("test2");
+    }
+}
